e37f76d3003c5e73a1c2f7a6427b2892c6f4a887
Jaden Geller <jaden.geller@gmail.com>
2016-04-12 19:23:01 -0700

    Basic color-changing UI

--- Burrow/iOS/ViewController.swift
+++ Burrow/iOS/ViewController.swift
@@ -22,10 +22,37 @@ class ViewController: UIViewController {
 
     
     @IBAction func tunnelToggle(sender: UISwitch) {
+        // THIS CODE IS LITERALLY BARF
+        // DON'T WORRY, I'LL REFACTOR WHEN WE GET A BETTER IDEA OF WHAT WE'D LIKE
+        
+        let lingerDuration = 0.1
+        let animationDuration = NSTimeInterval(UINavigationControllerHideShowBarDuration) * 2 + lingerDuration
+        let waitDuration = NSTimeInterval(UINavigationControllerHideShowBarDuration) + lingerDuration
+        
         if sender.on {
+            UIView.animateWithDuration(animationDuration) {
+                self.view.backgroundColor = .darkGrayColor()
+            }
+
+            self.navigationController?.setNavigationBarHidden(true, animated: true)
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(waitDuration * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+                self.navigationController?.setNavigationBarHidden(false, animated: true)
+                self.navigationController?.navigationBar.barStyle = .Black
+                self.title = "Burrow (Enabled)"
+            }
             print("User enabled tunnel.")
         } else {
+            UIView.animateWithDuration(animationDuration) {
+                self.view.backgroundColor = .whiteColor()
+            }
             print("User disabled tunnel.")
+            
+            self.navigationController?.setNavigationBarHidden(true, animated: true)
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(waitDuration * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+                self.navigationController?.setNavigationBarHidden(false, animated: true)
+                self.navigationController?.navigationBar.barStyle = .Default
+                self.title = "Burrow (Disabled)"
+            }
         }
     }
 }
