5e62ccf913a1cdf2d08c7cbcef77085c7df99e40
Jaden Geller <jaden.geller@gmail.com>
2016-05-09 02:26:06 -0700

    Added todo about private imports

--- Burrow/Shovel/Shovel.h
+++ Burrow/Shovel/Shovel.h
@@ -16,5 +16,6 @@ FOUNDATION_EXPORT const unsigned char ShovelVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Shovel/PublicHeader.h>
 
+// TODO: This could be privately imported. Not sure how though.
 #import "ResourceRecord.h"
 #import "ServerMessage.h"
