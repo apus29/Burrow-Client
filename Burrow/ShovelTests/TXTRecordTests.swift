99b3f1d71977a64d9c1f57c72465d8f8f1707bae
Jaden Geller <jaden.geller@gmail.com>
2016-05-30 01:04:42 -0700

    Fixed issue where some tests didn't use promises

--- Burrow/ShovelTests/TXTRecordTests.swift
+++ Burrow/ShovelTests/TXTRecordTests.swift
@@ -15,7 +15,7 @@ class TXTRecordTests: XCTestCase {
     func testConstant() {
         let expectation = expectationWithDescription("received")
         
-        try! DNSResolver.resolveTXT("constant.test.burrow.tech") { response in
+        try! DNSResolver.resolveTXT("constant.test.burrow.tech").then { response in
             let txtRecords = try! response.unwrap()
             
             XCTAssertEqual(["I am the constant record."], txtRecords.map { $0.contents })
@@ -35,7 +35,7 @@ class TXTRecordTests: XCTestCase {
 
         let expectation = expectationWithDescription("received")
 
-        try! DNSResolver.resolveTXT("babies.test.burrow.tech") { response in
+        try! DNSResolver.resolveTXT("babies.test.burrow.tech").then { response in
             let txtRecords = try! response.unwrap()
             
             XCTAssertEqual(expectedCount, txtRecords.count)
@@ -54,7 +54,7 @@ class TXTRecordTests: XCTestCase {
         let expectedMessage = "Bacon ipsum dolor amet frankfurter filet mignon tenderloin, jowl short loin corned beef jerky beef ribs spare ribs. Kevin bresaola venison jowl filet mignon. Turducken pork belly pig ball tip tail, alcatra brisket leberkas tri-tip fatback jerky pancetta filet mignon tenderloin. Landjaeger cupim drumstick rump shankle doner cow. Meatball prosciutto tri-tip, doner bresaola landjaeger ball tip andouille pork chop cupim ground round ribeye drumstick pastrami. Cow tenderloin picanha prosciutto pancetta, fatback andouille shoulder. Pig drumstick cow, landjaeger short loin chuck beef ribs. Andouille swine leberkas jowl ribeye doner biltong cupim ball tip prosciutto corned beef. T-bone sirloin filet mignon tongue alcatra shank pig short ribs pork belly tenderloin ribeye. Beef picanha pork t-bone bacon tail salami fatback frankfurter ribeye doner turducken. Porchetta doner rump short loin turducken tenderloin sausage pork. Tenderloin t-bone tri-tip shankle. Tri-tip ground round pork belly, landjaeger ham pancetta bresaola meatball ribeye strip steak pig alcatra. Alcatra sausage tri-tip biltong shoulder bresaola. Shankle swine cow, sausage brisket short loin picanha kielbasa turkey strip steak t-bone tongue hamburger. Shank ham hock pork loin, fatback alcatra andouille prosciutto short loin pastrami shankle hamburger. Boudin ham hamburger filet mignon bacon drumstick. Pork chop prosciutto capicola"
         let expectation = expectationWithDescription("received")
 
-        try! DNSResolver.resolveTXT("bacon.test.burrow.tech") { response in
+        try! DNSResolver.resolveTXT("bacon.test.burrow.tech").then { response in
             let txtRecords = try! response.unwrap()
             
             XCTAssertEqual([expectedMessage], txtRecords.map{ $0.contents })
